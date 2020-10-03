a43860a86cf6a242738f329c50bac61570a11d7e
Arthur <arthurcose@gmail.com>
2020-10-03 00:15:06 -0400

    Fix env variables for keepAwake

--- src/keepAwake.ts
+++ src/keepAwake.ts
@@ -1,20 +1,18 @@
 import Koa from "koa";
 import fetch from "node-fetch";
 
-if (process.env.$PORT) {
+if (process.env.PORT) {
   const app = new Koa();
 
   app.use((ctx) => {
     ctx.body = "I'm awake!";
   });
 
-  app.listen(process.env.$PORT);
+  app.listen(process.env.PORT);
 
   // server falls asleep every 30 mins, keep it awake by messaging every 20 mins
   setInterval(async () => {
-    const res = await fetch(
-      `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`
-    );
+    const res = await fetch(process.env.URL);
 
     console.log(await res.text());
   }, 20 * 60 * 1000);
