1b6aba172013f03c5a3eccb202b710a64882ab35
Arthur Cosentino <arthurcose@gmail.com>
2023-04-06 00:46:17 -0400

    Group tests

--- tests/test-findWatermarkY.ts
+++ tests/test-findWatermarkY.ts
@@ -5,25 +5,68 @@ import Jimp from "jimp";
 import findIfunnyWatermarkY from "../src/watermark-finders/ifunny";
 import findRedditWatermarkY from "../src/watermark-finders/reddit";
 
-const images = [];
+let ifunnyImages: Jimp[] = [];
+let redditImages: Jimp[] = [];
+let noWatermarkImages: Jimp[] = [];
 
-test.before(async () => {
-  const folderPath = "tests/files/";
+async function loadImages(folderPath: string): Promise<Jimp[]> {
   const imagePaths = await fs.readdir(folderPath);
   const imagePromises = imagePaths.map((path) => Jimp.read(folderPath + path));
-  const imagesPromise = await Promise.all(imagePromises);
 
-  images.push(...imagesPromise);
+  return await Promise.all(imagePromises);
+}
+
+test.before(async () => {
+  ifunnyImages = await loadImages("tests/files/ifunny/");
+  redditImages = await loadImages("tests/files/reddit/");
+  noWatermarkImages = await loadImages("tests/files/no-watermark/");
 });
 
+function countPassing(
+  images: Jimp[],
+  searchFunc: (image: Jimp) => number
+): number {
+  return images.filter((image) => searchFunc(image) > 0).length;
+}
+
+type PassingResults = {
+  reddit: number;
+  ifunny: number;
+  noWatermark: number;
+};
+
+function countEachPassing(
+  testName: string,
+  searchFunc: (image: Jimp) => number
+): PassingResults {
+  console.log(`-- ${testName} test, with ifunny images`);
+  const ifunny = countPassing(ifunnyImages, searchFunc);
+
+  console.log(`-- ${testName} test, with reddit images`);
+  const reddit = countPassing(redditImages, searchFunc);
+
+  console.log(`-- ${testName} test, with no-watermark images`);
+  const noWatermark = countPassing(noWatermarkImages, searchFunc);
+
+  return {
+    ifunny,
+    reddit,
+    noWatermark,
+  };
+}
+
 test("ifunny", (t) => {
-  console.log("ifunny");
-  const passing = images.filter((image) => findIfunnyWatermarkY(image) > 0);
-  t.is(passing.length, 8, "expect 8 passing");
+  const result = countEachPassing(t.title, findIfunnyWatermarkY);
+
+  t.is(result.ifunny, ifunnyImages.length, "expect all ifunny passing");
+  t.is(result.reddit, 0, "expect 0 reddit passing");
+  t.is(result.noWatermark, 0, "expect 0 no-watermark passing");
 });
 
 test("reddit", (t) => {
-  console.log("reddit");
-  const passing = images.filter((image) => findRedditWatermarkY(image) > 0);
-  t.is(passing.length, 3, "expect 3 passing");
+  const result = countEachPassing(t.title, findRedditWatermarkY);
+
+  t.is(result.reddit, redditImages.length, "expect all reddit passing");
+  t.is(result.ifunny, 0, "expect 0 ifunny passing");
+  t.is(result.noWatermark, 0, "expect 0 no-watermark passing");
 });
